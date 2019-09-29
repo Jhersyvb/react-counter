646823e2b43643d0f874174834ba1d883c7d0b99
Jhersy <jhersyvb@gmail.com>
2019-09-28 20:47:14 -0500

    Challenges completed

--- src/index.js
+++ src/index.js
@@ -6,18 +6,47 @@ import './styles.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [results, setResults] = useState([])
 
   const incrementCount = increment => {
     setCount(count + increment)
   }
 
+  const resetCount = () => {
+    setCount(0)
+  }
+
+  const saveResult = () => {
+    setResults(results.concat({
+      id: results.length,
+      value: count
+    }))
+    setCount(0)
+  }
+
   return (
     <div className="App">
-      <Button increment={1} onClickFunction={incrementCount} />
-      <Button increment={10} onClickFunction={incrementCount} />
-      <Button increment={100} onClickFunction={incrementCount} />
-      <Button increment={1000} onClickFunction={incrementCount} />
-      <span>{count}</span>
+      <p>
+        <Button increment={1} onClickFunction={incrementCount} />
+        <Button increment={10} onClickFunction={incrementCount} />
+        <Button increment={100} onClickFunction={incrementCount} />
+        <Button increment={1000} onClickFunction={incrementCount} />
+      </p>
+      <div>
+        {count} <button type="button" onClick={resetCount}>Reset</button>
+        <button type="button" onClick={saveResult}>Save</button>
+      </div>
+      <p>
+        <Button increment={-1} onClickFunction={incrementCount} />
+        <Button increment={-10} onClickFunction={incrementCount} />
+        <Button increment={-100} onClickFunction={incrementCount} />
+        <Button increment={-1000} onClickFunction={incrementCount} />
+      </p>
+      <ul>
+        {results.map(result => (
+          <li key={result.id}>{result.value}</li>
+        ))}
+      </ul>
     </div>
   )
 }
